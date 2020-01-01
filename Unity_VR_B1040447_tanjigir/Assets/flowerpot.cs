d92bf33d673b4d3b6800ace0ef7b32162f9e0d9a
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2020-01-01 22:12:43 +0800

    撰寫規則

---
+++ Unity_VR_B1040447_tanjigir/Assets/flowerpot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class flowerpot : MonoBehaviour
+{
+    public static bool flowerpotenter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "花盆")
+        {
+            flowerpotenter = true;
+
+            print("花盆");
+        }
+    }
+}
