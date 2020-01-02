f43de2531a682cb4926ef2a55d50d8902ef63e6d
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2020-01-02 14:37:46 +0800

    恭喜過關

---
+++ Unity_VR_B1040447_tanjigir/Assets/spirts/BigLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigLight : MonoBehaviour
+{
+    public static bool BigLightenter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "燈")
+        {
+            BigLightenter = true;
+
+            print("燈");
+        }
+    }
+}
---
+++ Unity_VR_B1040447_tanjigir/Assets/spirts/complete.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class complete : MonoBehaviour
+{
+    public GameObject end;
+
+    private void Update()
+    {
+        if (cup.cupenter && printer.printerenter && keyboard.keyboardenter && chair.chairenter && BigLight.BigLightenter && flowerpot.flowerpotenter)
+        {
+            end.SetActive(true);
+
+            print("enter");
+        }
+    }
+}
